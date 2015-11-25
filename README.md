e8fb4d5b09fe1ad38fe722a6187cf5288b841d7c
kepennar <kevin.pennarun@gmail.com>
2015-11-25 13:27:12 +0100

    Revert "components"
    This reverts commit a10d64bc8209c8171aa80b7f7f31492055f0e0d1.

--- README.md
+++
@@ -1,9 +0,0 @@
-# Préparation
-
-## Ajout du bundler
- Webpack
- 
-## Suppression de Bower au profit de NPM
- Moins d'outils automatisé, mais...
- 
- ## Utilisation du New-router
--- src/app/sections/article-details/article-details.module.js
+++ src/app/sections/article-details/article-details.module.js
@@ -3,8 +3,13 @@
 
   angular
     .module('front.article-details', [
+<<<<<<< HEAD
+			'ui.bootstrap',
+			'front.core.post'
+=======
 			'front.core.post',
 			'front.ui.rating',
 			'front.ui.comment'
+>>>>>>> a55ca7e... make directive working
 		]);
 })();
